49df89475be157889b5c6b4ef0506f2160ef79d9
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-07-12 19:33:59 +0530

    test

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const detailsRouter = require('./routes/details')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+app.use('/',function(req,res){
+    res.send("app is running")
+})
 app.use('/api',detailsRouter)
 
 app.listen(3001,()=>{
